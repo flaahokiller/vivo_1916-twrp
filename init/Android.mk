5186fc5a4a84a3e43f2ec3189dff077c196c1d89
Flashokiller <75789819+flaahokiller@users.noreply.github.com>
2021-04-18 12:08:20 +0530

    Update Android.mk

--- init/Android.mk
+++ init/Android.mk
@@ -22,7 +22,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := \
     system/core/base/include \
     system/core/init
-LOCAL_SRC_FILES := init_ginkgo.cpp
-LOCAL_MODULE := libinit_ginkgo
+LOCAL_SRC_FILES := init_1916.cpp
+LOCAL_MODULE := libinit_1916
 
 include $(BUILD_STATIC_LIBRARY)
